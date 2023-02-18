7c73865f36ae3df1a722dc53ba44959b41497952
Josh Bellingham <92236549+ChimingPork@users.noreply.github.com>
2023-02-18 12:59:20 -0800

    Initial Commit - Basic Setup of Landing Page

---
+++ src/Landing/Landing.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./Landing.css";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="title">
+          <h1 className="title maintext">Josh Bellingham</h1>
+          <h2 className="title subtext">Junior Web Developer</h2>
+          <button className="title btn">
+            <span>Welcome</span>
+          </button>
+        </div>
+        <div className="sun">
+          <img className="sun-img" src="Sun.png" alt="Sun" />
+          <div className="sun-ray r-one"></div>
+          <div className="sun-ray r-two"></div>
+          <div className="sun-ray r-three"></div>
+          <div className="sun-ray r-four"></div>
+        </div>
+        <div className="waves">
+          <img className="wave-1" src="Wave 1.png" alt="Wave"></img>
+          <img className="wave-2" src="Wave 2.png" alt="Wave"></img>
+          <img className="wave-3" src="Wave 3.png" alt="Wave"></img>
+        </div>
+      </div>
+    );
+  }
+}
